691cb6ca1778bc92d4f5f86234f37a92f81439c7
Ville Korhonen <ville@xd.fi>
2013-10-28 04:45:39 +0200

    Use ExaminationManager to detect current examination

--- exams/context_processors.py
+++ exams/context_processors.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-import datetime
-from django.utils.timezone import utc
-from django.db.models import Q
 
 from exams.models import Examination
 
 def current_examination(request):
-    now = datetime.datetime.utcnow().replace(tzinfo=utc)
-    current = Examination.objects.filter(Q(registration_begin__lte=now, registration_end__gte=now) | Q(registration_status='E')).order_by('-registration_end')
+    current = Examination.objects.get_active()
 
     if len(current) > 0:
         current = current[0]
