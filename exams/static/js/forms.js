200b44b770b125e19663bb9c52e957eee31b60c7
Ville Korhonen <ville@xd.fi>
2013-11-13 12:29:50 +0200

    When losing focus, set field value to 0 if no value

--- exams/static/js/forms.js
+++ exams/static/js/forms.js
@@ -2,6 +2,17 @@ function selectFirstField() {
     $('form:not(.filter) :input:visible:first').focus();
 }
 
+function fillEmptyAmount() {
+
+}
+
 $(document).ready(function() {
     selectFirstField();
+
+    $("input.amount").focusout(function() {
+        var val = $(this).val().trim();
+        if (val == '') {
+            $(this).val(0);
+        }
+    });
 });
