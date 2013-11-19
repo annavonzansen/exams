a2adb98f4fba6acfe4ad016eb099f3a76aa5e6c2
Ville Korhonen <ville@xd.fi>
2013-11-19 18:07:06 +0200

    Added translation support for models

--- exams/translation.py
+++ exams/translation.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 from modeltranslation.translator import translator, TranslationOptions
 
-from exams.models import Subject
+from exams.models import Subject, MaterialType, SubjectGroup, SpecialArrangement
 
 class SubjectTranslationOptions(TranslationOptions):
     fields = ('name',)
-#translator.register(Subject, SubjectTranslationOptions)
+translator.register(Subject, SubjectTranslationOptions)
+
+class SubjectGroupTranslationOptions(TranslationOptions):
+    fields = ('name',)
+translator.register(SubjectGroup, SubjectGroupTranslationOptions)
+
+class MaterialTypeTranslationOptions(TranslationOptions):
+    fields = ('title',)
+translator.register(MaterialType, MaterialTypeTranslationOptions)
+
+class SpecialArrangementTranslationOptions(TranslationOptions):
+    fields = ('title',)
+translator.register(SpecialArrangement, SpecialArrangementTranslationOptions)
